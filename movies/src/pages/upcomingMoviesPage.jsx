992208d570356ca44552f9c83f1f777f3ce32780
collegesaiah <20093594@mail.wit.ie>
2025-03-18 15:15:48 +0000

    new button for upcoming movies page

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -3,7 +3,7 @@ import { getUpcomingMovies } from "../api/tmdb-api";
 import PageTemplate from '../components/templateMovieListPage';
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../components/spinner';
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const UpcomingMoviesPage = (props) => {
   const { data, error, isPending, isError  } = useQuery({
@@ -26,7 +26,7 @@ const UpcomingMoviesPage = (props) => {
       title="Upcoming Movies"
       movies={upcomingMovies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
+        return <PlaylistAddIcon movie={movie} />
       }}
     />
   );
